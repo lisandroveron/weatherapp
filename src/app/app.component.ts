fded3ecb6ce190871cc1a9807af71af309ad281c
lisandroveron <lisandro.veron@hotmail.com>
2022-10-18 17:48:11 -0300

    Added: 'Card' component. Weather API is now connected. Style for all website.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-}
+	constructor(){
+		fetch("https://www.7timer.info/bin/api.pl?lon=-58.3&lat=-34.8&product=civil&output=json")
+		.then(response => response.json())
+		.then(api => api = api.dataseries);
+	};
+};
